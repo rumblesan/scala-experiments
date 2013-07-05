d27ba237d34b689bfeae52ff0186a9ea9030dbc0
Guy John <guy@rumblesan.com>
2013-07-05 14:22:51 +0100

    Setting main text message

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,6 +2,6 @@ package com.rumblesan.scalaexperiments
 
 object App {
   def main(args: Array[String]) {
-    print("Hello com.rumblesan.Scala Experiments!")
+    print("My Scala experiments, let me show you them.")
   }
 }
