fdf08809d30cbe280a11a09becfc7cbda03a5548
Guy John <guy@rumblesan.com>
2013-07-10 09:44:30 +0100

    Simplifying MyTree example

--- src/main/scala/AlgebraicDatatypes.scala
+++ src/main/scala/AlgebraicDatatypes.scala
@@ -11,28 +11,16 @@ object MyTree {
   def addData(data: String, kbucket: MyTree): MyTree = {
     kbucket match {
       case MyTreeEmpty => MyTreeLeaf(data)
-      case leaf: MyTreeLeaf => addData(data, leaf)
-      case tree: MyTreeTree => addData(data, tree)
-    }
-  }
-
-  def addData(data: String, leaf: MyTreeLeaf): MyTree = {
-    if (data < leaf.data) {
-      MyTreeTree(leaf.data, MyTreeLeaf(data), MyTreeEmpty)
-    } else if (data > leaf.data) {
-      MyTreeTree(leaf.data, MyTreeEmpty, MyTreeLeaf(data))
-    } else {
-      leaf
-    }
-  }
-
-  def addData(data: String, tree: MyTreeTree): MyTree = {
-    if (data < tree.data) {
-      MyTreeTree(tree.data, addData(data, tree.left), tree.right)
-    } else if (data > tree.data) {
-      MyTreeTree(tree.data, tree.left, addData(data, tree.right))
-    } else {
-      tree
+      case leaf: MyTreeLeaf => {
+        if (data < leaf.data) MyTreeTree(leaf.data, MyTreeLeaf(data), MyTreeEmpty)
+        else if (data > leaf.data) MyTreeTree(leaf.data, MyTreeEmpty, MyTreeLeaf(data))
+        else leaf
+      }
+      case tree: MyTreeTree => {
+        if (data < tree.data) MyTreeTree(tree.data, addData(data, tree.left), tree.right)
+        else if (data > tree.data) MyTreeTree(tree.data, tree.left, addData(data, tree.right))
+        else tree
+      }
     }
   }
 
