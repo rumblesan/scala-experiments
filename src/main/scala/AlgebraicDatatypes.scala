9031ba743116b40efecc20ec3b5ebf138b3cc3ef
Guy John <guy@rumblesan.com>
2013-07-10 16:06:19 +0100

    Sealing case classes

--- src/main/scala/AlgebraicDatatypes.scala
+++ src/main/scala/AlgebraicDatatypes.scala
@@ -4,8 +4,8 @@ import scalaz._, Scalaz._
 
 sealed trait MyTree[+T]
 case object MyTreeEmpty extends MyTree[Nothing]
-case class MyTreeLeaf[T: Order](data: T) extends MyTree[T]
-case class MyTreeTree[T: Order](data: T, left: MyTree[T], right: MyTree[T]) extends MyTree[T]
+sealed case class MyTreeLeaf[T: Order](data: T) extends MyTree[T]
+sealed case class MyTreeTree[T: Order](data: T, left: MyTree[T], right: MyTree[T]) extends MyTree[T]
 
 trait MyTreeOps {
 
