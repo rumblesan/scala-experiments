41f47468abc2b5b4a0062f4c5440c69a15fdeed2
Guy John <guy@rumblesan.com>
2013-07-16 23:27:15 +0100

    Adding example of \/ scalaz class

---
+++ src/main/scala/ScalazTypes/Validation.scala
@@ -0,0 +1,22 @@
+package com.rumblesan.scalaexperiments.scalaz.validation
+
+import scalaz._, Scalaz._
+
+
+object MyValidationStuff {
+
+
+  def likesEven(value: Int): \/[String, Int] = {
+    if (value % 2 == 0) value.right[String]
+    else "i hate odds".left[Int]
+  }
+
+  def likesNonZero(value: Int): \/[String, Int] = {
+    if (value != 0) value.right[String]
+    else "i hate zeros".left[Int]
+  }
+
+  def justTimes2(value: Int): Int = value * 2
+
+}
+
---
+++ src/test/scala/MyValidationStuffSpec.scala
@@ -0,0 +1,68 @@
+package com.rumblesan.scalaexperiments.validationtest
+
+import scalaz._, Scalaz._
+
+import org.specs2.mutable._
+
+import com.rumblesan.scalaexperiments.scalaz.validation._
+
+
+class MyValidationStuffSpec extends Specification {
+
+  "The 'Validationxtend Example'" should {
+    "pass a value through correctly using maps and flatmaps" in {
+
+      val validated = 2.right[String].flatMap(
+        v => MyValidationStuff.likesEven(v)
+      ).map(
+        v => MyValidationStuff.justTimes2(v)
+      ).flatMap(
+        v => MyValidationStuff.likesNonZero(v)
+      )
+
+      validated must_==(4.right[String])
+
+    }
+    "have a value trigger the first error" in {
+
+      val errord = 3.right[String].flatMap(
+        v => MyValidationStuff.likesEven(v)
+      ).map(
+        v => MyValidationStuff.justTimes2(v)
+      ).flatMap(
+        v => MyValidationStuff.likesNonZero(v)
+      )
+
+      errord.isLeft must beTrue
+      
+      errord.swap.getOrElse("") must_==("i hate odds")
+
+    }
+    "have a value trigger the second error" in {
+
+      val errord = 0.right[String].flatMap(
+        v => MyValidationStuff.likesEven(v)
+      ).map(
+        v => MyValidationStuff.justTimes2(v)
+      ).flatMap(
+        v => MyValidationStuff.likesNonZero(v)
+      )
+
+      errord.isLeft must beTrue
+      
+      errord.swap.getOrElse("") must_==("i hate zeros")
+
+    }
+    "work fine with for comprehensions" in {
+
+      val validated = for {
+        isEven <- MyValidationStuff.likesEven(2)
+        isMultiplied = MyValidationStuff.justTimes2(isEven)
+        isNotZero <- MyValidationStuff.likesNonZero(isMultiplied)
+      } yield isNotZero
+
+      validated must_==(4.right[String])
+
+    }
+  }
+}
