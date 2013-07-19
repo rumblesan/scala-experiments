566a5757e92dca68b1c5392fa0e3e80f53a0df56
Guy John <guy@rumblesan.com>
2013-07-19 17:10:06 +0100

    Making naming of objects clearer in typeclass example

--- src/main/scala/Typeclass.scala
+++ src/main/scala/Typeclass.scala
@@ -8,21 +8,22 @@ case class PaidUser(id: Long, name: String, email: String, payments: Double)
 
 case class UserInfo(name: String, email: String)
 
+
 object UserInfoMethods {
 
-  trait UserInfoLike[T] {
+  trait UserInfoTypeClass[T] {
     def getUserInfo(user: T): UserInfo
   }
-  object UserInfoLike {
-    implicit val UserInfoLikeBasic = new UserInfoLike[BasicUser] {
+  object UserInfoTypeClass {
+    implicit val UserInfoTypeClassBasic = new UserInfoTypeClass[BasicUser] {
       def getUserInfo(user: BasicUser): UserInfo = UserInfo(user.name, user.email)
     }
-    implicit object UserInfoLikePaid extends UserInfoLike[PaidUser] {
+    implicit object UserInfoLikeTypeClassPaid extends UserInfoTypeClass[PaidUser] {
       def getUserInfo(user: PaidUser): UserInfo = UserInfo(user.name, user.email)
     }
   }
 
-  def userInfo[T](user: T)(implicit tc: UserInfoLike[T]): UserInfo = tc.getUserInfo(user)
+  def userInfo[T](user: T)(implicit tc: UserInfoTypeClass[T]): UserInfo = tc.getUserInfo(user)
 
 }
 
