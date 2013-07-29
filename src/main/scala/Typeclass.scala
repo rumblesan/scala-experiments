22724a9d110c8807b31f0bf9254814367253130c
Guy John <guy@rumblesan.com>
2013-07-29 12:49:17 +0100

    Splitting the typeclass example apart more
    Trying to make things even clearer

--- src/main/scala/Typeclass.scala
+++ src/main/scala/Typeclass.scala
@@ -8,20 +8,21 @@ case class PaidUser(id: Long, name: String, email: String, payments: Double)
 
 case class UserInfo(name: String, email: String)
 
+trait UserInfoTypeClass[T] {
+  def getUserInfo(user: T): UserInfo
+}
+
+object UserInfoTypeClass {
+  implicit val UserInfoTypeClassBasic = new UserInfoTypeClass[BasicUser] {
+    def getUserInfo(user: BasicUser): UserInfo = UserInfo(user.name, user.email)
+  }
+  implicit object UserInfoLikeTypeClassPaid extends UserInfoTypeClass[PaidUser] {
+    def getUserInfo(user: PaidUser): UserInfo = UserInfo(user.name, user.email)
+  }
+}
+
 
 object UserInfoMethods {
-
-  trait UserInfoTypeClass[T] {
-    def getUserInfo(user: T): UserInfo
-  }
-  object UserInfoTypeClass {
-    implicit val UserInfoTypeClassBasic = new UserInfoTypeClass[BasicUser] {
-      def getUserInfo(user: BasicUser): UserInfo = UserInfo(user.name, user.email)
-    }
-    implicit object UserInfoLikeTypeClassPaid extends UserInfoTypeClass[PaidUser] {
-      def getUserInfo(user: PaidUser): UserInfo = UserInfo(user.name, user.email)
-    }
-  }
 
   def userInfo[T](user: T)(implicit tc: UserInfoTypeClass[T]): UserInfo = tc.getUserInfo(user)
 
