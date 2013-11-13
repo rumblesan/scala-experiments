94fb24dd025c18e2854c3d2fc40f2fd4307e0105
Guy John <guy@rumblesan.com>
2013-11-13 16:50:18 +0000

    Rewriting Equal Spec to use scala check

--- src/test/scala/MyTree/MyTreeEqualSpec.scala
+++ src/test/scala/MyTree/MyTreeEqualSpec.scala
@@ -1,15 +1,21 @@
 package com.rumblesan.scalaexperiments.tests.mytree
 
-import org.specs2.mutable._
+import org.specs2.mutable.Specification
+import org.specs2.ScalaCheck
+import org.scalacheck.{Arbitrary, Gen, Prop}
+
+import Arbitrary.arbitrary
+import Prop.forAll
 
 import scalaz._, Scalaz._
 
 import com.rumblesan.scalaexperiments.mytree._
 
 
-class MyTreeEqualSpec extends Specification {
+class MyTreeEqualSpec extends Specification with ScalaCheck {
 
   "The MyTree Equal type class" should {
+
     "say two empty trees are equal" in {
 
       val tree1: MyTree[Int] = MyTreeEmpty
@@ -18,20 +24,29 @@ class MyTreeEqualSpec extends Specification {
       (tree1 === tree2) must beTrue
 
     }
-    "say two simple trees are equal" in {
+    "say two simple trees are equal" in forAll { number: Int =>
 
-      val tree1: MyTree[Int] = MyTreeLeaf(1)
-      val tree2: MyTree[Int] = MyTreeLeaf(1)
+      val tree1: MyTree[Int] = MyTreeLeaf(number)
+      val tree2: MyTree[Int] = MyTreeLeaf(number)
 
       (tree1 === tree2) must beTrue
 
     }
-    "say two different trees are not equal" in {
+    "two trees with the same numbers should be equal" in forAll { numbers: List[Int] =>
 
-      val tree1: MyTree[Int] = MyTreeTree(1, MyTreeLeaf(1), MyTreeLeaf(1))
-      val tree2: MyTree[Int] = MyTreeTree(1, MyTreeLeaf(1), MyTreeLeaf(2))
+      val tree1 = numbers.foldLeft(MyTreeEmpty: MyTree[Int])((t, n) => t.addData(n))
+      val tree2 = numbers.foldLeft(MyTreeEmpty: MyTree[Int])((t, n) => t.addData(n))
 
-      (tree1 === tree2) must beFalse
+      (tree1 === tree2) must beTrue
+
+    }
+    "say two different trees are not equal" in forAll { (l1: List[Int], l2: List[Int]) =>
+
+      val tree1 = l1.foldLeft(MyTreeEmpty: MyTree[Int])((t, n) => t.addData(n))
+      val tree2 = l2.foldLeft(MyTreeEmpty: MyTree[Int])((t, n) => t.addData(n))
+
+      if (l1 === l2) (tree1 === tree2) must beTrue
+      else (tree1 === tree2) must beFalse
 
     }
   }
