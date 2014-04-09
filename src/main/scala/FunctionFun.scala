f1a83fc87f0535c4580a26ba695a53fe38a342e3
Guy John <guy@rumblesan.com>
2014-04-09 12:03:02 +0100

    Adding a couple of basic partial function examples

--- src/main/scala/FunctionFun.scala
+++ src/main/scala/FunctionFun.scala
@@ -6,6 +6,12 @@ object FunctionFun {
     a + b + c
   }
 
+  def valueFunction1(a: Int): Int = a * 3
+
+  val valueFunction2: Int => Int = a => a * 3
+
+  def secondOrderFunc(input: Int, func: Int => Int): Int = func(input)
+
 }
 
 
--- src/test/scala/FunctionFunSpec.scala
+++ src/test/scala/FunctionFunSpec.scala
@@ -15,5 +15,26 @@ class FunctionFunSpec extends Specification {
       result must_==(9)
 
     }
+
+    "show how vals and defs can be used as first order functions" in {
+
+      val result1 = secondOrderFunc(3, valueFunction1)
+      val result2 = secondOrderFunc(3, valueFunction2)
+
+      result1 must_==(result2)
+    }
+
+    "show how vals and defs are different if you partialy apply them" in {
+
+      val fns = List(valueFunction1 _, valueFunction2)
+
+      val results = fns.map((fn: Int => Int) =>
+        fn(3)
+      )
+
+      results must_==(List(9, 9))
+
+    }
+
   }
 }
